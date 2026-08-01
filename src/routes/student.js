edbe979da3a160ac7ed2b391afa7b1ea8e07d688
CodeWithMB <mohan243200s@gmail.com>
2026-08-01 23:51:53 +0530

    added login post request endpoint route

--- src/routes/student.js
+++ src/routes/student.js
@@ -1,6 +1,7 @@
-import registerStudent from "../controllers/studentController.js";
+import { registerStudent, getStudents, loginStudent } from "../controllers/studentController.js";
 import express from "express";
 const router = express.Router();
 router.post("/register", registerStudent);
-
+router.get("/getStudents", getStudents);
+router.post("/login", loginStudent)
 export default router;
