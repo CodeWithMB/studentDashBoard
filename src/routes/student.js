2d38d67ba892a20b0c0b8192285b8f87cb93ed24
CodeWithMB <mohan243200s@gmail.com>
2026-08-03 18:45:12 +0530

    added protection to dashboard routes using middleware

--- src/routes/student.js
+++ src/routes/student.js
@@ -1,7 +1,9 @@
-import { registerStudent, getStudents, loginStudent } from "../controllers/studentController.js";
+import { registerStudent, getStudents, loginStudent, dashboard} from "../controllers/studentController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 import express from "express";
 const router = express.Router();
 router.post("/register", registerStudent);
 router.get("/getStudents", getStudents);
 router.post("/login", loginStudent)
+router.get("/dashboard", authMiddleware, dashboard);
 export default router;
