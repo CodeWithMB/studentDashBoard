792057c974f53b1af04acd81f5df8d71e43896ba
CodeWithMB <mohan243200s@gmail.com>
2026-08-05 12:53:55 +0530

    Added routing to many api files at once

--- src/routes/student.js
+++ src/routes/student.js
@@ -1,4 +1,4 @@
-import { registerStudent, getStudents, loginStudent, dashboard} from "../controllers/studentController.js";
+import { registerStudent, getStudents, loginStudent, dashboard, subject, showProfile, editProfile, changePassword} from "../controllers/studentController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import express from "express";
 const router = express.Router();
@@ -6,4 +6,8 @@ router.post("/register", registerStudent);
 router.get("/getStudents", getStudents);
 router.post("/login", loginStudent)
 router.get("/dashboard", authMiddleware, dashboard);
+router.get('/showProfile', authMiddleware, showProfile);
+router.put("/editProfile", authMiddleware, editProfile);
+router.put("/changePassword", authMiddleware, changePassword);
+router.get("/subject", authMiddleware, subject);
 export default router;
